1989513c72ab0be41827197806760595ef2949c6
bloxrantblixrant-crypto <bloxrantblixrant@gmail.com>
2026-07-27 19:49:58 +0200

    Refactor Firebase.js for improved imports and config

--- Firebase.js
+++ Firebase.js
@@ -1,44 +1,26 @@
-// Firebase imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import { 
-    getAuth 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-import { 
-    getFirestore 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import { 
-    getStorage 
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+import { initializeApp } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 
-// Firebase configuration
-
-const firebaseConfig = {
-
-apiKey: "AIzaSyD4LPNv50zWVq9qwcqMkQqn_srxY5FEh5E",
-
-authDomain: "roblox-creator-hub.firebaseapp.com",
-
-projectId: "roblox-creator-hub",
-
-storageBucket: "roblox-creator-hub.firebasestorage.app",
-
-messagingSenderId: "644590740742",
-
-appId: "1:644590740742:web:528b21c65e38bed7b42a87"
-
-};
+import { getAuth } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 
-// Initialize Firebase
+import { getFirestore } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+import { getStorage } 
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
+
+import { firebaseConfig }
+from "./firebase-config.js";
+
+
 
 const app = initializeApp(firebaseConfig);
 
-
-// Firebase services
 
 const auth = getAuth(app);
 
@@ -47,7 +29,6 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 
-// Export for other files
 
 export {
     auth,
