1c659503b1e69c28d9674b07579d34d261b2ded3
bloxrantblixrant-crypto <bloxrantblixrant@gmail.com>
2026-07-27 19:49:08 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyD4LPNv50zWVq9qwcqMkQqn_srxY5FEh5E",
+
+    authDomain: "roblox-creator-hub.firebaseapp.com",
+
+    projectId: "roblox-creator-hub",
+
+    storageBucket: "roblox-creator-hub.firebasestorage.app",
+
+    messagingSenderId: "644590740742",
+
+    appId: "1:644590740742:web:528b21c65e38bed7b42a87"
+
+};
